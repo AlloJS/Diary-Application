a0504fffcb474332ab21a538df21d857b94425ec
AlloJS <eventorganization1@gmail.com>
2024-08-17 19:33:23 +0200

    Continuazione dei template e filtri su eventi, separazione tra dettaglio e titolo evento

--- app/routes.py
+++ app/routes.py
@@ -9,19 +9,37 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@bp.route('/dashboard',methods=['POST'])
+@bp.route('/dashboard',methods=['GET'])
 def dashboard():
-    if request.method == 'POST':
+    if request.method == 'GET':
         try:
-            id_diary = request.form.get('id_diary')
-            d = diary.diary.diary_DB .DiarySQL('default','localhost','Diary','root','root')
+            message = ""
+            id_diary = request.args.get('id_diary')
+            d = diary.diary.diary_DB .DiarySQL(host='localhost',database='Diary',user='root',password='root')
             d.start_diary(id_diary)
             d.orderby_startdata('crescente')
+            # Diario esistente 65592250285068942839
+            d.set_name(id_diary)
+            name_diary = d.name
             list_events = d.diary
-            return render_template('dashboard.html',list_events=list_events)
-        except ValueError as e:
+            name_event = request.args.get('search-event')
+            print("name_event ->")
+            print(name_event)
+            if name_event is not None:
+                list_events = [diz for diz in list_events if name_event in diz['name']]
+                if len(list_events) == 0:
+                    message = "Nessun evento trovato."
+
+            return render_template('dashboard.html',id_diary=id_diary,name_diary=name_diary,list_events=list_events,message=message)
+        except  ValueError as e:
             err = e
             return render_template('index.html',err=err)
+
+@bp.route('/aggiungiEvento', methods=['GET'])
+def aggiungi_evento():
+    id_diary = request.args.get('id_diary')
+    return render_template('aggiungiEvento.html',id_diary=id_diary)
+
 
 
 
